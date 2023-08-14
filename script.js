a99b9e619113f636c4a60b41cc9a40647db26904
Niraj Khatri <nirajkhatri0601@gmail.com>
2023-08-14 19:50:36 -0400

    Added a "Loading" indicator when displaying random quotes. Also updated the CSS and HTML
    Improve user experience with a newly added "loading" indicator, and enhance text readability with some new CSS.

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 const QUOTABLE_API_URL = "https://api.quotable.io/random";
 
+// Cache DOM elements
+const quoteContainer = document.getElementById("quote-container");
+const quoteText = document.getElementById("quote");
+const authorText = document.getElementById("author");
+const generateButton = document.getElementById("generate-button");
+const loading = document.getElementById("loading"); // New addition
+
 async function fetchRandomQuote() {
     try {
         const response = await fetch(QUOTABLE_API_URL);
@@ -20,15 +27,22 @@ async function fetchRandomQuote() {
     }
 }
 
-const quoteContainer = document.getElementById("quote-container");
-const quoteText = document.getElementById("quote");
-const authorText = document.getElementById("author");
-const generateButton = document.getElementById("generate-button");
-
 async function displayRandomQuote() {
-    const quoteData = await fetchRandomQuote();
-    quoteText.textContent = quoteData.content;
-    authorText.textContent = "- " + quoteData.author;
+    generateButton.disabled = true; // Disable the button while loading
+    loading.style.display = 'block'; // Show the loading indicator
+
+    try {
+        const quoteData = await fetchRandomQuote();
+        quoteText.textContent = quoteData.content;
+        authorText.textContent = "- " + quoteData.author;
+    } catch (error) {
+        console.error("Error fetching quote:", error);
+        quoteText.textContent = "Failed to fetch a quote. Please try again.";
+        authorText.textContent = "";
+    } finally {
+        loading.style.display = 'none'; // Hide the loading indicator
+        generateButton.disabled = false; // Re-enable the button
+    }
 }
 
 generateButton.addEventListener("click", displayRandomQuote);
