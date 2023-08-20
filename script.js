d12028deae77f5558dc6fcff91b82344154ea2ec
Niraj Khatri <nirajkhatri0601@gmail.com>
2023-08-19 23:34:53 -0400

    Implemented capability to fetch and display a background image based on the random quote's content
    Using the Unsplash API, a random image is fetched and set as the background, which is based on the content of the qutoe.

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const QUOTABLE_API_URL = "https://api.quotable.io/random";
+const UNSPLASH_ACCESS_KEY = "UNSPLASH_API_KEY";
 
 // Cache DOM elements
 const quoteContainer = document.getElementById("quote-container");
@@ -11,6 +12,7 @@ async function fetchRandomQuote() {
     try {
         const response = await fetch(QUOTABLE_API_URL);
         if (!response.ok) {
+            console.error("Response status:", response.status);
             throw new Error("Failed to fetch quote");
         }
         const data = await response.json();
@@ -25,6 +27,21 @@ async function fetchRandomQuote() {
             author: "",
         };
     }
+    
+}
+
+async function generateImageBasedOnQuote(quoteText) {
+    const query = encodeURIComponent(quoteText);
+    const response = await fetch(
+        `https://api.unsplash.com/search/photos?query=${query}&per_page=1&client_id=${UNSPLASH_ACCESS_KEY}`
+    );
+    const data = await response.json();
+
+    if (data.results.length > 0) {
+        return data.results[0].urls.regular;
+    } else {
+        return "URL_TO_DEFAULT_IMAGE";
+    }
 }
 
 async function displayRandomQuote() {
@@ -35,6 +52,11 @@ async function displayRandomQuote() {
         const quoteData = await fetchRandomQuote();
         quoteText.textContent = quoteData.content;
         authorText.textContent = "- " + quoteData.author;
+
+        const randomImageUrl = await generateImageBasedOnQuote(quoteData.content);
+        const backgroundImage = document.getElementById("background-image"); // Select the background image container
+        backgroundImage.style.backgroundImage = `url(${randomImageUrl})`;
+
     } catch (error) {
         console.error("Error fetching quote:", error);
         quoteText.textContent = "Failed to fetch a quote. Please try again.";
