501e222bcc6486b931774eb6bafb97fa6565e033
Niraj <96727369+Nizzy0@users.noreply.github.com>
2023-08-20 17:40:13 -0400

    Update script.js
    API problem

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const QUOTABLE_API_URL = "https://api.quotable.io/random";
-const UNSPLASH_ACCESS_KEY = "UNSPLASH_API_KEY";
+const UNSPLASH_ACCESS_KEY = "lEj8M4SFKrWX8O_plVsJwDWXf1MkEkjkzq_DV1N4GTk";
 
 // Cache DOM elements
 const quoteContainer = document.getElementById("quote-container");
