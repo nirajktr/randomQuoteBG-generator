f92e45cbf83bde13d58caf6baa4eea990d6046c1
Niraj Khatri <nirajkhatri0601@gmail.com>
2023-09-20 16:59:47 -0400

    Refactor code for generating image,  and comments for clarity
    LG.

--- script.js
+++ script.js
@@ -8,33 +8,38 @@ const authorText = document.getElementById("author");
 const generateButton = document.getElementById("generate-button");
 const loading = document.getElementById("loading");
 
+// Function to generate a random image URL from Unsplash
 async function generateRandomImage() {
+    // Fetch a random image from Unsplash
     const response = await fetch(
         `https://api.unsplash.com/photos/random?count=1&client_id=${UNSPLASH_ACCESS_KEY}`
     );
     const data = await response.json();
 
     if (data.length > 0) {
-        return data[0].urls.regular;
+        return data[0].urls.regular; // Return the regular image URL
     } else {
         return "https://static.vecteezy.com/system/resources/previews/005/572/340/original/foggy-landscape-forest-in-the-morning-beautiful-sunrise-mist-cover-mountain-background-at-countryside-winter-free-photo.jpg"; // Set a fallback image URL here
     }
 }
 
+// Function to generate an image based on a given quote
 async function generateImageBasedOnQuote(quoteText) {
     const query = encodeURIComponent(quoteText);
+    // Search for an image on Unsplash related to the quote
     const response = await fetch(
         `https://api.unsplash.com/search/photos?query=${query}&per_page=1&client_id=${UNSPLASH_ACCESS_KEY}`
     );
     const data = await response.json();
 
     if (data.results.length > 0) {
-        return data.results[0].urls.regular;
+        return data.results[0].urls.regular; // Return the regular image URL
     } else {
-        return generateRandomImage();
+        return generateRandomImage(); // If no related image found, use a random image
     }
 }
 
+// Function to fetch a random quote from the Quotable API
 async function fetchRandomQuote() {
     try {
         const response = await fetch(QUOTABLE_API_URL);
@@ -55,6 +60,7 @@ async function fetchRandomQuote() {
     }
 }
 
+// Function to display a random quote and associated image
 async function displayRandomQuote() {
     generateButton.disabled = true; // Disable the button while loading
     loading.style.display = 'block'; // Show the loading indicator
@@ -77,7 +83,8 @@ async function displayRandomQuote() {
     }
 }
 
+// Add an event listener to the "Generate Quote" button
 generateButton.addEventListener("click", displayRandomQuote);
 
-// Initial quote
+// Initial quote display when the page loads
 displayRandomQuote();
